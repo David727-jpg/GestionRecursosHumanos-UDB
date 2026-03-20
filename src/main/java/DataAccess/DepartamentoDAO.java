617d69456e077938996e762c7ffce0efedb66647
KrissiaFer <krissiaeguizabal@gmail.com>
2026-03-19 22:18:30 -0600

    Update DepartamentoDAO.java

--- src/main/java/DataAccess/DepartamentoDAO.java
+++ src/main/java/DataAccess/DepartamentoDAO.java
@@ -10,7 +10,7 @@ import java.sql.ResultSet;
 import com.udb.modelo.Departamento;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.SQLIntegrityConstraintViolationException;
+
 
 /**
  *
